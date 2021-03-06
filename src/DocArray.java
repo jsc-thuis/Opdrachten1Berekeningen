575cb7485479288af21384124b15607f854f9eab
Jos <jozef.casteels@thomasmore.be>
2021-03-06 10:28:05 +0100

    Opdrachten 1 Berekeningen met Arrays OPGAVEN

---
+++ src/DocArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class DocArray {
+    public float mediaan(int[] getallen) {
+        float med = 0;
+        return med;
+    }
+
+    public float gemiddelde(int[] getallen) {
+        float gem = 0;
+        return gem;
+    }
+
+    public float standaarddeviatie(int[] getallen) {
+        float stddev = 0;
+        return stddev;
+    }
+}
+
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        DocArray da = new DocArray();
+        System.out.println("We testen de methodes uit de klasse DocArray...");
+    }
+}
