4531bb649742556ca5984a51c421b04fea65fae9
Jos <jozef.casteels@thomasmore.be>
2021-03-06 10:37:16 +0100

    Opdrachten 1 Berekeningen met Arrays OPLOSSING

--- src/DocArray.java
+++ src/DocArray.java
@@ -3,16 +3,47 @@ import java.util.Arrays;
 public class DocArray {
     public float mediaan(int[] getallen) {
         float med = 0;
+        int aantal = getallen.length;
+        Arrays.sort(getallen);
+        if (aantal % 2 == 0) {
+            med = (float) (getallen[aantal / 2 - 1] + getallen[aantal / 2]) / 2;   // even aantal
+        } else {
+            med = getallen[aantal / 2];    // oneven aantal
+        }
         return med;
     }
 
     public float gemiddelde(int[] getallen) {
-        float gem = 0;
+        int som = 0;
+        int lengte = getallen.length;
+
+        // met foreach-lus de som van alle getalllen berekenen
+        for (int getal : getallen) {
+            som += getal;
+        }
+//      kan ook berekend worden met gewone for-lus
+//        for (int i = 0; i < lengte; i++) {
+//            som += getallen[i];
+//        }
+
+        float gem = (float) som / lengte;    // CASTEN: int som wordt float (p623)
+        // dit zorgt ervoor dat som/lengte als resultaat een float geeft en geen integer
         return gem;
     }
 
     public float standaarddeviatie(int[] getallen) {
+        float som = 0;
+        float gem = gemiddelde(getallen);
         float stddev = 0;
+        int aantal = getallen.length;
+
+        // som berekenen van kwadraten van afwijkingen
+        for (int getal : getallen) {
+            som += Math.pow(getal - gem, 2);    // kwadraat
+        }
+
+        // deze som delen door het aantal en de vierkantswortel nemen
+        stddev = (float) Math.sqrt(som / aantal);  // vierkantswortel
         return stddev;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,20 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         DocArray da = new DocArray();
-        System.out.println("We testen de methodes uit de klasse DocArray...");
+
+        // mediaan van een oneven aantal getallen
+        int oneven[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(oneven));
+        System.out.println("mediaan: " + da.mediaan(oneven));
+
+        // mediaan van een even aantal getallen
+        int even[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(Arrays.toString(even));
+        System.out.println("mediaan: " + da.mediaan(even));
+
+        // standaarddeviatie van een reeks getallen
+        int resultaten[] = {2, 4, 5, 5, 6, 7, 9, 10};
+        System.out.println(Arrays.toString(resultaten));
+        System.out.println("standaarddeviatie: " + da.standaarddeviatie(resultaten));
     }
 }
